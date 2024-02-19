b8ec323f21eea98113a1e240a5e789eb77252afa
dangar16 <al415624@uji.es>
2024-02-19 15:30:18 +0100

    container fluid

--- src/Amigurumis.js
+++ src/Amigurumis.js
@@ -1,16 +1,19 @@
 export default function Amigurumis(){
     return (
-        <div className="col" style={{backgroundColor:"#E1F0DA"}}>
-            <div className="row">
-                <h1 className="text-center">Qué son los Amigurumis</h1>
-            </div>
-            <div className="row">
-                    <p style={{textAlign: "justify"}}>
-                        Los amigurumis son adorables muñecos tejidos a mano que se originaron en Japón. 
-                        Su nombre proviene de la combinación de las palabras ami (tejido) y nuigurumi (muñeco de peluche). 
-                        Estos pequeños compañeros conquistan corazones con su ternura y estilo único, convirtiéndolos en objetos de colección y regalos apreciados por personas de todas las edades.
-                    </p>                
+        <div className="container-fluid" style={{backgroundColor:"#E1F0DA"}}>
+            <div className="col">
+                <div className="row">
+                    <h1 className="text-center">Qué son los Amigurumis</h1>
+                </div>
+                <div className="row">
+                        <p style={{textAlign: "justify"}}>
+                            Los amigurumis son adorables muñecos tejidos a mano que se originaron en Japón. 
+                            Su nombre proviene de la combinación de las palabras ami (tejido) y nuigurumi (muñeco de peluche). 
+                            Estos pequeños compañeros conquistan corazones con su ternura y estilo único, convirtiéndolos en objetos de colección y regalos apreciados por personas de todas las edades.
+                        </p>                
+                </div>
             </div>
         </div>
+        
     )
 }
